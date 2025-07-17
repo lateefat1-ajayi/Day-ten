6f108b6e549bd86ec64cc794e8600548432a9a1a
lateefat <ajayilateefat09@gmail.com>
2025-07-16 18:00:56 -0700

    deployment issuse ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Recipes from './pages/Recipes';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-white text-gray-800 font-sans">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recipes" element={<Recipes />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+      </Routes>
     </BrowserRouter>
   );
 }
